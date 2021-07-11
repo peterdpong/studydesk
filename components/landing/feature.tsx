a1519ae3f0fcad15c979e96a01fdc4a2551bcbff
Soraphoj(Peter) Damrongpiriyapong <peterdpong@gmail.com>
2021-07-10 21:53:23 -0400

    Updated landing page with navbar, footer, and new feature elements

---
+++ components/landing/feature.tsx
@@ -0,0 +1,28 @@
+import { ReactElement } from 'react';
+import { Text, Stack, Flex } from '@chakra-ui/react';
+
+interface FeatureProps {
+  title: string;
+  text: string;
+  icon: ReactElement;
+}
+
+export default function Feature({ title, text, icon }: FeatureProps) {
+  return (
+    <Stack>
+      <Flex
+        w={16}
+        h={16}
+        align={'center'}
+        justify={'center'}
+        color={'white'}
+        rounded={'full'}
+        bg={'gray.100'}
+        mb={1}>
+        {icon}
+      </Flex>
+      <Text fontWeight={600}>{title}</Text>
+      <Text color={'gray.600'}>{text}</Text>
+    </Stack>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
-
 import {
   Flex,
   Stack,
@@ -10,10 +9,19 @@ import {
   Text,
   Button,
   Box,
-  Image
+  Image,
+  SimpleGrid,
+  Icon,
+  Center,
+  HStack
 } from '@chakra-ui/react'
 
-import { ArrowForwardIcon } from '@chakra-ui/icons'
+import { FcBullish, FcCalendar, FcTodoList } from 'react-icons/fc';
+import { BiBookOpen } from "react-icons/bi";
+import Navbar from '../components/landing/navbar'
+import Footer from '../components/landing/footer'
+import Feature from '../components/landing/feature'
+import { ArrowForwardIcon } from '@chakra-ui/icons';
 
 export default function Home() {
   return (
@@ -24,13 +32,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main>
+        <Navbar/>
+
         <Flex
           align="center"
           justify={{ base: "center", md: "space-around", xl: "space-between" }}
           direction={{ base: "column-reverse", md: "row" }}
           flexWrap="nowrap"
           minH="70vh"
+          my="5"
         >
           <Stack
             spacing={2}
@@ -57,12 +68,12 @@ export default function Home() {
               The ultimate student dashboard.
             </Heading>
             <Text
-              size="lg"
+              fontSize="lg"
               textAlign={["center", "center", "left", "left"]}
             >
               Organize all your classes the way you want it. Track tasks, files, marks and tests all in one place.
             </Text>
-            <Link href="#">
+            {/* <Link href="#">
               <Button 
               rightIcon={<ArrowForwardIcon/>} 
               colorScheme="blue" 
@@ -70,13 +81,185 @@ export default function Home() {
               >
                 Try it
               </Button>
-            </Link>
+            </Link> */}
+            <Text
+              fontSize="xl"
+              textAlign={["center", "center", "left", "left"]}
+            >
+              <i>Coming soon.</i>
+            </Text>
+
           </Stack>
           <Box w={{ base: "80%", sm: "60%", md: "50%" }} mb={{ base: 12, md: 0 }}>
             <Image src="/images/placeholder.jpeg" size="100%" rounded="1rem" shadow="xl" />
           </Box>
 
         </Flex>
+
+        <Box 
+          p={10}
+        >
+          <Heading
+            mt={5}
+            size={'2xl'}
+            textAlign={'center'}
+          >
+            Built by a students for students.
+          </Heading>
+          <Text
+            mb={8}
+            textAlign={'center'}
+          >
+            Studydesk's features help organize all aspects of school.
+          </Text>
+          <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10} my={'5'}>
+            <Feature
+              icon={<Icon as={FcTodoList} w={10} h={10} />}
+              title={'Organize school tasks'}
+              text={
+                'All the tasks you need to do in one place, organized by class and priority. Focus on class specific task list or get an overview with a global task list.'
+              }
+            />
+            <Feature
+              icon={<Icon as={FcCalendar} w={10} h={10} />}
+              title={'Manage your school timetable'}
+              text={
+                'Input your school timetable and keep track of lectures and tutorials. Plan future timetables with a mock timetable tool.'
+              }
+            />
+            <Feature
+              icon={<Icon as={FcBullish} w={10} h={10} />}
+              title={'Marks tracking'}
+              text={
+                'Stop tracking with your marks with a spreadsheet. Check your current average and calculate what your need to get your target grade.'
+              }
+            />
+          </SimpleGrid>
+          <Center>
+            <Link href="#">
+                <Button 
+                leftIcon={<BiBookOpen/>} 
+                colorScheme="blue" 
+                variant="solid"
+                >
+                  Learn more
+                </Button>
+            </Link> 
+          </Center>
+
+        </Box>
+
+
+        
+        <Box
+          p={10}
+        >
+          <Heading
+            my={5}
+            size={'2xl'}
+            textAlign={'center'}
+          >
+            How Studydesk can help organize.
+          </Heading>
+          
+          <Center>
+            <HStack
+              my={'5'}
+            >
+              <Flex
+                w={{base: 16, md: 24}}
+                h={{base: 16, md: 24}}
+                align={'center'}
+                justify={'center'}
+                color={'white'}
+                rounded={'full'}
+                bg={'gray.100'}
+                >
+                <Icon as={FcTodoList} w={{base: 10, md: 12}} h={{base: 10, md: 12}} />
+              </Flex>
+              <Heading
+                size={'lg'}
+              >
+                Global and class focused task lists.
+              </Heading>
+            </HStack>
+          </Center>
+
+          <Center>
+            <HStack
+              my={'5'}
+            >
+              <Flex
+                w={{base: 16, md: 24}}
+                h={{base: 16, md: 24}}
+                align={'center'}
+                justify={'center'}
+                color={'white'}
+                rounded={'full'}
+                bg={'gray.100'}
+                >
+                <Icon as={FcCalendar} w={{base: 10, md: 12}} h={{base: 10, md: 12}} />
+              </Flex>
+              <Heading
+                size={'lg'}
+              >
+                Load and plan your timetables.
+              </Heading>
+            </HStack>
+          </Center>
+
+          <Center>
+            <HStack
+              my={'5'}
+            >
+              <Flex
+                w={{base: 16, md: 24}}
+                h={{base: 16, md: 24}}
+                align={'center'}
+                justify={'center'}
+                color={'white'}
+                rounded={'full'}
+                bg={'gray.100'}
+                >
+                <Icon as={FcBullish} w={{base: 10, md: 12}} h={{base: 10, md: 12}} />
+              </Flex>
+              <Heading
+                size={'lg'}
+              >
+                Manage your marks and calculate minimum grades
+              </Heading>
+            </HStack>
+          </Center>
+
+        </Box>
+
+        <Box
+          p={10}
+          alignContent={'center'}
+        >
+          <Heading
+            size={'2xl'}
+            m={5}
+            textAlign={'center'}
+          >
+            Try Studydesk today.
+          </Heading>
+          <Center>
+            <Link href="#">
+                <Button 
+                rightIcon={<ArrowForwardIcon/>} 
+                colorScheme="blue" 
+                variant="solid"
+                >
+                  Sign up.
+                </Button>
+            </Link> 
+          </Center>
+          
+        </Box>
+
+        <Footer/>
+
       </main>
       
     </div>
