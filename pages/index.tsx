636996bbde88aca0f670698f473c7dc394633cb4
Peter Damrongpiriyapong <peterdpong@gmail.com>
2022-08-22 23:48:09 -0400

    Fix import build error

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,10 +16,10 @@ import {
   Container,
 } from '@chakra-ui/react'
 
-import Navbar from '../components/landing/Navbar'
-import Footer from '../components/landing/Footer'
 import { ArrowForwardIcon } from '@chakra-ui/icons';
 import Hero from '../components/landing/Hero';
+import Navbar from '../components/landing/Navbar';
+import Footer from '../components/landing/Footer';
 
 export default function Home() {
   return (
