62f435ac4dbc537ad595c3b4fc370b45ae8a0ddb
Peter Damrongpiriyapong <peterdpong@gmail.com>
2022-12-19 14:11:11 -0500

    Landing page changes

--- components/landing/Footer.tsx
+++ components/landing/Footer.tsx
@@ -8,19 +8,18 @@ import {
   Text,
   useColorModeValue,
   VisuallyHidden,
-} from '@chakra-ui/react';
-import { FaTwitter } from 'react-icons/fa';
-import { ReactNode } from 'react';
-
+} from '@chakra-ui/react'
+import { FaTwitter } from 'react-icons/fa'
+import { ReactNode } from 'react'
 
 const SocialButton = ({
   children,
   label,
   href,
 }: {
-  children: ReactNode;
-  label: string;
-  href: string;
+  children: ReactNode
+  label: string
+  href: string
 }) => {
   return (
     <chakra.button
@@ -37,41 +36,41 @@ const SocialButton = ({
       transition={'background 0.3s ease'}
       _hover={{
         bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-      }}>
+      }}
+    >
       <VisuallyHidden>{label}</VisuallyHidden>
       {children}
     </chakra.button>
-  );
-};
+  )
+}
 
 export default function Footer() {
   return (
     <Box
       bg={useColorModeValue('gray.50', 'gray.900')}
-      color={useColorModeValue('gray.700', 'gray.200')}>
+      color={useColorModeValue('gray.700', 'gray.200')}
+    >
       <Container
         as={Stack}
         maxW={'6xl'}
         py={4}
         spacing={4}
         justify={'center'}
-        alignItems="center">
-        <Heading
-          size="md"
-        >
-          Studydesk
-        </Heading>
+        alignItems="center"
+      >
+        <Heading size="md">Studydesk</Heading>
         <Stack direction={'row'} spacing={6}>
-          <Link href={'#'}>Home</Link>
-          <Link href={'#'}>Features</Link>
-          <Link href={'#'}>About</Link>
+          <Link href={'#'}>Privacy</Link>
+          <Link href={'#'}>Terms</Link>
+          <Link href={'#'}>Contact</Link>
         </Stack>
       </Container>
 
       <Box
         borderTopWidth={1}
         borderStyle={'solid'}
-        borderColor={useColorModeValue('gray.200', 'gray.700')}>
+        borderColor={useColorModeValue('gray.200', 'gray.700')}
+      >
         <Container
           as={Stack}
           maxW={'6xl'}
@@ -79,15 +78,19 @@ export default function Footer() {
           direction={{ base: 'column', md: 'row' }}
           spacing={4}
           justify={{ base: 'center', md: 'space-between' }}
-          align={{ base: 'center', md: 'center' }}>
-          <Text>Created in Toronto 🇨🇦</Text>
+          align={{ base: 'center', md: 'center' }}
+        >
+          <Text>Studydesk - Built in Canada.</Text>
           <Stack direction={'row'} spacing={6}>
-            <SocialButton label={'Twitter'} href={'#'}>
+            <SocialButton
+              label={'Twitter'}
+              href={'https://twitter.com/trystudydesk'}
+            >
               <FaTwitter />
             </SocialButton>
           </Stack>
         </Container>
       </Box>
     </Box>
-  );
+  )
 }
